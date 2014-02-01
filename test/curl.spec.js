dee6846cd727ac525514e703efc36a46be4c3bb2
rprieto <choicesmade@gmail.com>
2014-02-01 16:33:57 +1100

    Better CURL support

--- test/curl.spec.js
+++ test/curl.spec.js
@@ -5,12 +5,12 @@ describe('curl', function() {
 
   describe('GET', function() {
 
-    it('without headers', function() {
+    it('to a given URL', function() {
       var cmd = curl.fromRequest({
         method: 'GET',
-        path: '/foo'
+        path: '/foo/bar?with=params'
       });
-      cmd.should.eql('curl -X GET http://localhost/foo');
+      cmd.should.eql('curl -X GET "http://localhost/foo/bar?with=params"');
     });
 
     it('with headers', function() {
@@ -22,7 +22,23 @@ describe('curl', function() {
           'X-Custom': '1234'
         }
       });
-      cmd.should.eql('curl -X GET -H "Accept: text/plain" -H "X-Custom: 1234" http://localhost/foo');
+      cmd.should.eql('curl -X GET -H "Accept: text/plain" -H "X-Custom: 1234" "http://localhost/foo"');
+    });
+
+  });
+
+  describe('POST', function() {
+
+    it('with a payload', function() {
+      var cmd = curl.fromRequest({
+        method: 'POST',
+        path: '/foo',
+        data: {
+          hello: 'world',
+          value: 123,
+        }
+      });
+      cmd.should.eql('curl -X POST -d \'{"hello":"world","value":123}\' "http://localhost/foo"');
     });
 
   });
