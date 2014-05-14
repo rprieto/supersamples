ad723cae2e24fcb099abab3f888d3a2746da7605
Romain Prieto <romain.prieto@gmail.com>
2014-05-15 08:50:18 +1000

    Support for raw buffer payloads + file attachments (instrumenting superagent)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var shim = require('./lib/shim');
 
 console.log('');
+shim.register('superagent', require('./lib/instruments/superagent'));
 shim.register('supertest', require('./lib/instruments/supertest'));
 shim.register('restify', require('./lib/instruments/restify'));
 
